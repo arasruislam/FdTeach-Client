9de455634fac9c1c6e7281be0c6e8f5762f996d8
asru-islam <arasru01@gmail.com>
2023-06-07 03:16:41 +0600

    add a private routes code and create a custom took.

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,9 +1,24 @@
-const PrivateRoute = () => {
+import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading) {
     return (
-        <div>
-            {/* Code Here */}
-        </div>
+      <button className="btn">
+        <span className="loading loading-spinner"></span>
+        loading
+      </button>
     );
+  }
+
+  if (user) {
+    return children;
+  }
+
+  return <Navigate to="/login" state={{ from: location }} replace />;
 };
 
 export default PrivateRoute;
