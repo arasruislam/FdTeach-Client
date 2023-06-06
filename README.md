983bc0224f30d717c84d4c1188db4e996b58b51a
asru-islam <arasru01@gmail.com>
2023-06-07 01:59:31 +0600

    add firebase configaration.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# b712-summer-camp-client-side-asru-islam
+# FdTeach Client Side
+
+* [Live Website]
+* [Live Server]
+* [Server Side Repo](https://github.com/programming-hero-web-course1/b7a12-summer-camp-server_side-asru-islam)
+
+## Learn From This Project
+
+### Fonts Collect
+
+### Image Collect
+
+### uses packages
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,8 +2,6 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
-  apiKey: import.meta.env.VITE_apiKey,
-  apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
   projectId: import.meta.env.VITE_projectId,
   storageBucket: import.meta.env.VITE_storageBucket,
