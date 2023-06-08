04084bf776c2395f1ea5f121c6927971fbc1c5c3
asru-islam <arasru01@gmail.com>
2023-06-08 09:29:06 +0600

    loginexisting user

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,20 +1,43 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import login from "../../assets/login.jpg";
 import SocialLogin from "../Shared/SocialLogin/SocialLogin";
+import useAuth from "../../hooks/useAuth";
+import { toast } from "react-hot-toast";
 
 const Login = () => {
+  const { loginExitingUser } = useAuth();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const from = location.state?.from?.pathname || "/";
+
+  const userLoginHandler = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    loginExitingUser(email, password)
+      .then(() => {
+        toast.success("User login successfully");
+        navigate(from, { replace: true });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="my-8">
       <div className="grid grid-cols-1 lg:grid-cols-2 place-items-center bg-white shadow-lg rounded-lg py-4 lg:p-8 gap-8 border-2 border-blue-50">
         <div className="card rounded-none w-full bg-base-100">
           <h1 className="text-center text-4xl font-bold">Login Now</h1>
-          <div className="card-body">
+          <form onSubmit={userLoginHandler} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
-                type="text"
+                type="email"
+                name="email"
                 placeholder="email"
                 className="input input-bordered"
               />
@@ -24,7 +47,8 @@ const Login = () => {
                 <span className="label-text">Password</span>
               </label>
               <input
-                type="text"
+                type="password"
+                name="password"
                 placeholder="password"
                 className="input input-bordered"
               />
@@ -45,10 +69,13 @@ const Login = () => {
             {/* Redirect register page */}
             <div>
               <small>
-                Do not have an <Link to="/signup" className="underline font-bold">signup now</Link>
+                Do not have an{" "}
+                <Link to="/signup" className="underline font-bold">
+                  signup now
+                </Link>
               </small>
             </div>
-          </div>
+          </form>
           <SocialLogin title={"Login With"} />
         </div>
         <div>
